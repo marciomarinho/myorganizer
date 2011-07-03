0711cd9d26af512739e91c68c5515c5c7d8a11fd
Marcio Alves Marinho <marciomarinho@gmail.com>
2011-07-03 17:49:36 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myorganizer::Application.config.secret_token = 'adedaf9972008979888619ae6f5c2dd3732488f6a220f5941c7fb7a53d58d4096f2063286dd27d536730f5058d5c21e8e9cb684eeeec1673d87f05e0c926d129'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Myorganizer::Application.config.session_store :cookie_store, :key => '_myorganizer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Myorganizer::Application.config.session_store :active_record_store
